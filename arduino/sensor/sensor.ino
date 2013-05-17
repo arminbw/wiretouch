4593119a32bafa5d616695b0417cbde45645258c
Georg Kaindl <gkaindl@mac.com>
2013-05-17 16:32:29 +0200

    cleaned arduino firmware + a TODO.

--- arduino/sensor/sensor.ino
+++ arduino/sensor/sensor.ino
@@ -1,23 +1,19 @@
 #include <SPI.h>
 
-#define SER_BUF_SIZE 704
-// 768
-#define PRINT_BINARY    1
+#define SERIAL_BAUD     1000000   // serial baud rate
+#define SER_BUF_SIZE    256       // serial buffer for sending
+#define IBUF_LEN        12        // serial buffer for incoming commands
 
-#define IBUF_LEN        12
+#define ORDER_MEASURE_UNORDERED   0
+#define PRINT_BINARY              1
+#define measure                   measure_with_atmega_adc
 
-// defines for setting and clearing register bits
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 #endif
 #ifndef sbi
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
-
-//#define DEBUG_PIN_UP()    PORTD |= (1 << 4)
-//#define DEBUG_PIN_DOWN()  PORTD &= ~(1 << 4)
-
-#define NUM_SELECT 4
 
 byte potValues[] = {
   210, 210, 210, 210, 210, 210, 210, 210,
@@ -26,46 +22,21 @@ byte potValues[] = {
   210, 210, 210, 210, 210, 210, 210, 210
 };
 
-const byte verticalShiftRegPins[] = {
-  9,             // latch / RCLK     PORTB.1  
-  8,             // clock / SRCLK    PORTB.0
-  4              // data  / SERs     PORTD.4
-};
-
-const byte verticalPosLeft[] = { 
-  15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-const byte verticalPosRight[] = { 
-  15, 14, 13, 12, 11, 10, 9, 8, 6, 5, 4, 3, 2, 1, 0, 7}; 
 const byte verticalWires = 32;
-
-const byte horizontalShiftRegPins[] = {
-  6,              // latch / RCLK  PORTD.6
-  5,              // clock / SRCLK   PORrTD.5
-  7               // data  / SER    PORTD.7
-};
-
-const byte horizontalPosTop[] = { 
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-const byte horizontalPosBottom[] = { 
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 const byte horizontalWires = 22;
 
 static uint8_t sbuf[SER_BUF_SIZE+1];
 static uint16_t sbufpos = 0;
 
-
-void setup() {
-  Serial.begin(1000000);
+void
+setup()
+{
+  Serial.begin(SERIAL_BAUD);
   
   // set prescale to 16
   cbi(ADCSRA,ADPS2);
   sbi(ADCSRA,ADPS1);
   cbi(ADCSRA,ADPS0);
-
-  for (int i=0; i<3; i++) {
-    pinMode(verticalShiftRegPins[i], OUTPUT);
-    pinMode(horizontalShiftRegPins[i], OUTPUT);
-  }
 
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
@@ -83,13 +54,15 @@ void setup() {
   pinMode(3, OUTPUT);
   TCCR2A = B00100011;
   TCCR2B = B11001;
-  OCR2A = 26;//26;
-  OCR2B = 13;//13;
+  OCR2A = 26;
+  OCR2B = 13;
   
   PORTD |= (1 << 2);
 }
 
-void muxSPI(byte output, byte vertical, byte off) {
+inline void
+muxSPI(byte output, byte vertical, byte off)
+{
   if (vertical)
     PORTB &= ~(1<<1);
   else
@@ -102,9 +75,6 @@ void muxSPI(byte output, byte vertical, byte off) {
        bits = 0xff;
     else {
       bits = ((output % 2) ? (1 << 4) : (1 << 5)) | (output >> 1);
-      // previous good one: bits = ((~(1 << (output / 16))) << 4) | (output % 16);
-      //bits = ((~(1 << ((output / 8)))) << 3) | (output % 8);
-      //bits = ((~(1 << ((output / 8)))) << 3) | ((15 < output) ? (output % 8) : (7 - (output % 8)))                   ;
     }
   } else {
     if (off)
@@ -133,21 +103,14 @@ void muxSPI(byte output, byte vertical, byte off) {
   }
 }
 
-unsigned int measure_with_atmega_adc() {
-  int val = 0;
-
-  for (int v=0; v<1; v++) {
-    unsigned rd = analogRead(0);
-    // if (rd > val)
-    val = rd; // (val >> 1) + (rd >> 1);
-  }
-  
-  return (val);
+inline unsigned int
+measure_with_atmega_adc()
+{
+  return analogRead(0);
 }
 
-#define measure measure_with_atmega_adc
-
-void send_packed10(uint16_t w16, byte flush_all)
+inline void
+send_packed10(uint16_t w16, byte flush_all)
 {
   static byte p = 0;
   static uint16_t d16 = 0;
@@ -174,18 +137,24 @@ void send_packed10(uint16_t w16, byte flush_all)
   }
 }
 
-void map_coords(uint16_t x, uint16_t y, uint16_t* mx, uint16_t* my)
+inline void
+map_coords(uint16_t x, uint16_t y, uint16_t* mx, uint16_t* my)
 {
-  /*uint16_t a = x * horizontalWires + y;
+#if ORDER_MEASURE_UNORDERED
+  uint16_t a = x * horizontalWires + y;
   uint16_t b = (59*a + 13) % (horizontalWires*verticalWires);
   
   *mx = b / horizontalWires;
-  *my = b - (*mx) * ((uint16_t)horizontalWires);*/
+  *my = b - (*mx) * ((uint16_t)horizontalWires);
+#else
   *mx = x;
-  *my = y; // (y % 2) ? 8 : 9;//y | 1;//y & ~1;//(y % 2) ? 8 : 9;
+  *my = y;
+#endif
 }
 
-void process_cmd(char* cmd) {
+void
+process_cmd(char* cmd)
+{
   switch (*cmd++) {
     case 'e': {
       byte column = 0, value = 0;
@@ -208,7 +177,9 @@ void process_cmd(char* cmd) {
   }
 }
 
-void loop() {
+void
+loop()
+{
   static boolean isRunning = 0;
   static byte ibuf_pos = 0;
   static char ibuf[IBUF_LEN];
@@ -218,8 +189,6 @@ void loop() {
     if (Serial.available()) {
       byte c = Serial.read();
       isRunning = ('s' == c);
-      /*if (isRunning)
-       attachInterrupt(1, measureInterrupt, FALLING);*/
     }
   }
   
@@ -244,12 +213,10 @@ void loop() {
        
       map_coords(k, l, &xx, &yy);
 
-      muxSPI(xx, 1, 0);
+      muxSPI(xx, 1, 0); // TODO: move out of loop
       muxSPI(yy, 0, 0);
       
-      PORTC &= ~(1 << 1); // analog pin 1
-
-      //delayMicroseconds(100);
+      PORTC &= ~(1 << 1);
 
       sample = measure_with_atmega_adc();
 
