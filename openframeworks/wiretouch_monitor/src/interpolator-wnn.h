37838b0f9094c1529b2ee93377daeda262818cdc
Armin Wagner <noreply@arminbwagner.com>
2013-08-23 19:34:54 +0200

    more licensing

--- openframeworks/wiretouch_monitor/src/interpolator-wnn.h
+++ openframeworks/wiretouch_monitor/src/interpolator-wnn.h
@@ -6,6 +6,25 @@
 //
 //
 
+/* Wiretouch: an open capacitive multi-touch tracker
+ * Copyright (C) 2011-2013 Georg Kaindl and Armin Wagner
+ *
+ * This file is part of Wiretouch
+ *
+ * Wiretouch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wiretouch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rfm12b-linux. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __wiretouch_monitor__interpolator_wnn__
 #define __wiretouch_monitor__interpolator_wnn__
 
--- openframeworks/wiretouch_monitor/src/main.cpp
+++ openframeworks/wiretouch_monitor/src/main.cpp
@@ -1,12 +1,29 @@
+/* Wiretouch: an open capacitive multi-touch tracker
+ * Copyright (C) 2011-2013 Georg Kaindl and Armin Wagner
+ *
+ * This file is part of Wiretouch
+ *
+ * Wiretouch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wiretouch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rfm12b-linux. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ofMain.h"
 #include "wtmApp.h"
 #include "ofAppGlutWindow.h"
 
-//========================================================================
 int main( ){
-
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, WINDOWWIDTH, WINDOWHEIGHT, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, WINDOWWIDTH, WINDOWHEIGHT, OF_WINDOW);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
